178aa1be98a11b4c8ea24652f98fd9849ab8d8e8
AndriYss <54698780+AndriYss@users.noreply.github.com>
2025-12-12 19:36:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Mencegah form dari submit default
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
+
+    // Cek username dan password
+    if (username === 'admin' && password === 'ayogs07') {
+        // Set session storage untuk menandakan pengguna sudah login
+        sessionStorage.setItem('authenticated', 'true');
+        // Arahkan ke halaman home
+        window.location.href = 'home.html';
+    } else {
+        errorMessage.innerText = 'Username atau Password salah';
+    }
+});
